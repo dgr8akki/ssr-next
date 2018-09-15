e1272de9f989a45754447f4e7326a6a3d1337c32
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-15 18:17:56 +0530

    Add about us page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
-const Index = () => {
-  return (
-    <div>
-      <p> Hello Next.js </p>
-    </div>
-  );
-};
+const Index = () => (
+  <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
+);
 
 export default Index;
